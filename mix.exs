3ced28190d364ee5ef7600e1bdbbe550a68daeea
Francesco Ceccon <francesco@longrockcapital.co.uk>
2017-10-10 14:42:28 +0100

    Add coveralls

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule Gdex.Mixfile do
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     test_coverage: [tool: ExCoveralls],
      deps: deps(),
      name: "Gdex",
      source_url: "https://github.com/fracek/gdex"]
@@ -20,6 +21,7 @@ defmodule Gdex.Mixfile do
     [{:httpoison, "~> 0.13"},
      {:poison, "~> 3.0"},
      {:dialyze, "~> 0.2", only: :dev, runtime: false},
-     {:ex_doc, "~> 0.16", only: :dev, runtime: false}]
+     {:ex_doc, "~> 0.16", only: :dev, runtime: false},
+     {:excoveralls, "~> 0.7", only: :test}]
   end
 end
